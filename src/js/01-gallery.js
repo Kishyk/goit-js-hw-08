025c7fca4b105498b34b0fa7d41a7aab68518bff
Kishyk <123320655+Kishyk@users.noreply.github.com>
2023-06-29 21:05:02 +0300

    трігер коміт для запуску деплою на гіті

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,19 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items.js'
+import {galleryItems} from './gallery-items.js'
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css'
 
-// Change code below this line
 
 const galleryList = document.querySelector('.gallery');
+
 
 let galleryItemsElement = "";
 galleryItems.forEach((item) => {
   galleryItemsElement = galleryItemsElement + `<li class="gallery__item">
    <a class="gallery__link" href="${item.original}">
-      <img 
-      class="gallery__image" 
-      src="${item.preview}" 
-      alt="${item.description}" 
+      <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
       />
    </a>
 </li>`
@@ -22,7 +21,7 @@ galleryItems.forEach((item) => {
 
 galleryList.insertAdjacentHTML('beforeend', galleryItemsElement);
 
-const lightbox = new SimpleLightbox('.gallery__link', {
+new SimpleLightbox('.gallery__link', {
   captionsData: 'alt',
   captionDelay: 250,
 });
